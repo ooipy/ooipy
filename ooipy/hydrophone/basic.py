398db7efb2349c398df3d6a11bd3a158ba09dcf8
FelixSchwock <fschwock@uw.edu>
2020-12-18 10:51:17 -0800

    added welch percentile - cleanup necessary

--- ooipy/hydrophone/basic.py
+++ ooipy/hydrophone/basic.py
@@ -290,6 +290,157 @@ class HydrophoneData(Trace):
                                            np.array(specgram))
             return self.spectrogram
 
+
+    def compute_spectrogram_wp(self, win='hann', L=4096, avg_time=None,
+                            overlap=0.5, verbose=True, percentile=0.5):
+        """
+        Compute spectrogram of acoustic signal. For each time step of the
+        spectrogram either a modified periodogram (avg_time=None)
+        or a power spectral density estimate using Welch's method with median
+        averaging is computed.
+
+        Parameters
+        ----------
+        win : str, optional
+            Window function used to taper the data. See scipy.signal.get_window
+            for a list of possible window functions (Default is Hann-window.)
+        L : int, optional
+            Length of each data block for computing the FFT (Default is 4096).
+        avg_time : float, optional
+            Time in seconds that is covered in one time step of the
+            spectrogram. Default value is None and one time step covers L
+            samples. If the signal covers a long time period it is recommended
+            to use a higher value for avg_time to avoid memory overflows and
+            to facilitate visualization.
+        overlap : float, optional
+            Percentage of overlap between adjacent blocks if Welch's method is
+            used. Parameter is ignored if avg_time is None. (Default is 50%)
+        verbose : bool, optional
+            If true (defult), exception messages and some comments are printed.
+
+        Returns
+        -------
+        Spectrogram
+            A Spectrogram object that contains time and frequency bins as
+            well as corresponding values. If no noise date is available,
+            None is returned.
+        """
+        specgram = []
+        time = []
+
+        if any(self.data) is None:
+            if verbose:
+                print('Data object is empty. Spectrogram cannot be computed')
+            self.spectrogram = None
+            return None
+
+        # sampling frequency
+        fs = self.stats.sampling_rate
+
+        # number of time steps
+        if avg_time is None:
+            nbins = int((len(self.data) - L) / ((1 - overlap) * L)) + 1
+        else:
+            nbins = int(np.ceil(len(self.data) / (avg_time * fs)))
+
+        # compute spectrogram. For avg_time=None
+        # (periodogram for each time step), the last data samples are ignored
+        # if len(noise[0].data) != k * L
+        if avg_time is None:
+            n_hop = int(L * (1 - overlap))
+            for n in range(nbins):
+                #f, Pxx, Nb = welch_percentile(x=self.data[n * n_hop:n * n_hop + L],
+                #    bias_func=bias_digamma_approx, fs=fs, window=win, nperseg=L,
+                #    overlap=0.5, nfft=L, percentile=percentile)
+                f, Pxx = signal.periodogram(
+                    x=self.data[n * n_hop:n * n_hop + L],
+                    fs=fs, window=win)
+                if len(Pxx) != int(L / 2) + 1:
+                    if verbose:
+                        print('Error while computing periodogram for segment',
+                              n)
+                    self.spectrogram = None
+                    return None
+                else:
+                    calib_time = self.stats.starttime.datetime
+                    tmp = self.freq_dependent_sensitivity_correct(
+                        int(L / 2 + 1), self.stats.location, calib_time)
+
+                    Pxx = 10 * np.log10(Pxx * np.power(10, tmp / 10)) - 128.9
+
+                    specgram.append(Pxx)
+                    time.append(self.stats.starttime.datetime
+                                + datetime.timedelta(seconds=n * L / fs))
+
+        else:
+            for n in range(nbins - 1):
+                f, Pxx, Nb = welch_percentile(x=self.data[n * int(fs * avg_time):
+                                (n + 1) * int(fs * avg_time)],
+                    bias_func=bias_digamma_approx, fs=fs, window=win, nperseg=L,
+                    overlap=0.5, nfft=L, percentile=percentile)
+                #f, Pxx = signal.welch(
+                #    x=self.data[n * int(fs * avg_time):
+                #                (n + 1) * int(fs * avg_time)],
+                #    fs=fs, window=win, nperseg=L, noverlap=int(L * overlap),
+                #    nfft=L, average='median')
+
+                if len(Pxx) != int(L / 2) + 1:
+                    if verbose:
+                        print('Error while computing '
+                              'Welch estimate for segment', n)
+                    self.spectrogram = None
+                    return None
+                else:
+                    calib_time = self.stats.starttime.datetime
+                    tmp = self.freq_dependent_sensitivity_correct(
+                        int(L / 2 + 1), self.stats.location, calib_time)
+
+                    Pxx = 10 * np.log10(Pxx * np.power(10, tmp / 10)) - 128.9
+                    specgram.append(Pxx)
+                    time.append(self.stats.starttime.datetime
+                                + datetime.timedelta(seconds=n * avg_time))
+
+            # compute PSD for residual segment
+            # if segment has more than L samples
+            if len(self.data[int((nbins - 1) * fs * avg_time):]) >= L:
+                f, Pxx, Nb = welch_percentile(x=self.data[int((nbins - 1) * fs * avg_time):],
+                    bias_func=bias_digamma_approx, fs=fs, window=win, nperseg=L,
+                    overlap=0.5, nfft=L, percentile=percentile)
+                #f, Pxx = signal.welch(
+                #    x=self.data[int((nbins - 1) * fs * avg_time):],
+                #    fs=fs, window=win, nperseg=L, noverlap=int(L * overlap),
+                #    nfft=L, average='median')
+                if len(Pxx) != int(L / 2) + 1:
+                    if verbose:
+                        print(
+                            'Error while computing Welch '
+                            'estimate residual segment')
+                    self.spectrogram = None
+                    return None
+                else:
+                    calib_time = self.stats.starttime.datetime
+                    tmp = self.freq_dependent_sensitivity_correct(
+                        int(L / 2 + 1), self.stats.location, calib_time)
+
+                    Pxx = 10 * np.log10(Pxx * np.power(10, tmp / 10)) - 128.9
+                    specgram.append(Pxx)
+                    time.append(self.stats.starttime.datetime
+                                + datetime.timedelta(seconds=(nbins - 1)
+                                                     * avg_time))
+
+        if len(time) == 0:
+            if verbose:
+                print('Spectrogram does not contain any data')
+            self.spectrogram = None
+            return None
+        else:
+            self.spectrogram = Spectrogram(np.array(time), np.array(f),
+                                           np.array(specgram))
+            return self.spectrogram
+
+
+
+
     def compute_spectrogram_mp(self, n_process=None, win='hann', L=4096,
                                avg_time=None, overlap=0.5, verbose=True):
         """
@@ -911,3 +1062,78 @@ class Psd:
         please see :meth:`ooipy.hydrophone.basic.plot_psd`
         '''
         ooipy.tools.ooiplotlib.plot_psd(self, **kwargs)
+
+
+def compute_nu(N, Ns, win, o):
+    Nb = int(N/(Ns*(1-o)))
+    def _sum_win(win, Ns, m, o):
+        win_s = np.roll(win, -int((1-o)*Ns)*m)
+        win_s[-int((1-o)*Ns)*m:] = 0
+        return abs(np.sum(win * win_s))**2
+    sum_denom = 0
+    for m in np.linspace(1, Nb-1, Nb-1, dtype=int):
+        sum_denom += (1 - m/Nb)*_sum_win(win, Ns, m, o)
+    nu = 2*Nb / (1 + 2 * sum_denom)
+    return nu
+
+# parts are from https://github.com/scipy/scipy/blob/v1.5.2/scipy/signal/spectral.py#L291-L454
+def welch_percentile(x, bias_func, fs=1.0, window='hann', nperseg=None, overlap=None, nfft=None,
+        detrend='constant', return_onesided=True, scaling='density',
+        axis=-1, percentile=None, numRV='edof'):
+
+    freqs, _, Pxy = signal.spectral._spectral_helper(x, x, fs, window, nperseg, int(nperseg * overlap), nfft,
+                                            detrend, return_onesided, scaling, axis,
+                                            mode='psd')
+    
+    Nb = Pxy.shape[-1]
+
+    if len(Pxy.shape) >= 2 and Pxy.size > 0:
+        if Pxy.shape[-1] > 1:
+            if percentile == None:
+                Pxy = Pxy.mean(axis=-1)
+            elif isinstance(percentile, int) or isinstance(percentile, float):
+                if percentile >= 0 and percentile <= 1:
+                    
+                    if numRV == 'n':
+                        Pxy = np.quantile(Pxy, percentile, axis=-1) / bias_func(Pxy.shape[-1], percentile)
+                    elif numRV == 'edof':
+                        win = signal.get_window(window, nperseg) / np.sqrt(np.sum(signal.get_window(window, nperseg)**2))
+                        edof = compute_nu(len(x), nperseg, win, overlap)
+                        if bias_func == bias_digamma_approx:
+                            Pxy = np.quantile(Pxy, percentile, axis=-1) / bias_func(edof / 2, percentile)
+                        else:
+                            Pxy = np.quantile(Pxy, percentile, axis=-1) / bias_func(int(np.round(edof / 2)), percentile)
+                else:
+                    raise ValueError('percentile must be between 0 and 1, got %s'
+                                    % (percentile,))
+            else:
+                raise ValueError('percentile must be integer, float, or None type, got type %s'
+                                    % (type(percentile),))
+        else:
+            Pxy = np.reshape(Pxy, Pxy.shape[:-1])
+    
+    return freqs, Pxy, Nb
+
+def bias_alternating_harmonic_series(N, p):
+    l = np.linspace(1, N, N)
+    return np.sum((-1)**(l+1)/l)
+
+def bias_truncated_harmonic_series(N, p):
+    percentiles = np.round(np.linspace(1/(N+1), 1 - 1/(N+1), N), 3)
+    if p in percentiles:
+        l = np.arange(np.round((N-1)*(1-p)) + 1, N+1, 1)
+    else:
+        l = np.arange(np.round(N*(1-p)) + 1, N+2, 1)
+    return np.sum(1/l)
+
+def bias_digamma_approx(N, p):
+    def digamma(x):
+        return np.log(x) - 1/(2*x) - 1/(12*x**2) + 1/(120*x**4) - 1/(252*x**6)
+    
+    return digamma(N+2) - digamma(N*(1-p)+1)
+
+def no_bias_correct(N, p):
+    return 1.0
+
+def bias_limit(N, p):
+    return -np.log(1-p)
